388d47d2c3a746fa86b7ad10e4d69a07b3b54981
Driisonims <ibrahimidrisonims@gmail.com>
2022-12-20 14:57:58 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,8 +11,7 @@ class Square:
             size (int): size of the square
         """
         if type(size) is not int:
-    
-            raise TypeError('size must be an integer')
+           raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
